af4367baaec2bcd9904f8f3be6ea6baf1d6c0118
bwall4 <bwall4@gmu.edu>
2024-04-21 13:25:25 -0400

    Cleanup button_wiring_test.c

--- src/subapps/button_wiring_test.c
+++ src/subapps/button_wiring_test.c
@@ -1,49 +1,9 @@
-#include "gpio.h"
+#include <stdio.h>
 
+#include "gpio.h"
 #include "types.h"
 
 #define COUNT 24
-
-int pin_nuasdfsdfmbers[COUNT] = {
-	75,
-	77,
-	79
-};
-
-int piasdfn_numbers[COUNT] = {
-	75,
-	77,
-	79,
-	80,
-	81,
-	11,
-	89,
-	88,
-	61,
-	//33,
-	//37,
-	//63
-	/* BANNED! 74,*/
-	76,
-	78,
-	8,
-
-	9,
-	10,
-	87,
-	86,
-	
-	22,
-	27,
-	47,
-	23,
-	45,
-
-	65,//40,//36, /* ? */
-
-	51,//51,//32, /* ? */
-	60,//60,//62, /* ? */
-};
 
 int pin_numbers[COUNT] = {
 	110,
@@ -72,8 +32,6 @@ int pin_numbers[COUNT] = {
 	86
 };
 
-// some that don't work: 125, 13, 123, 12, 40, 62
-// note: pin 5 does work, although my wire broke
 
 gpio_pin pins[COUNT];
 
@@ -85,13 +43,16 @@ int main_bwt(int arg, char **argv) {
 		has_been_true[i] = false;
 	}
 
+	/* The idea: Keep track of the set of pins that we have seen working, and
+	 * print out all the ones we haven't seen. That way, identifying pins
+	 * that don't work is as easy as pushing all the buttons and seeing
+	 * which pin numbers remain. */
 	for(;;) {
 		for(int i = 0; i < COUNT; ++i) {
 			bool state = gpio_read(pins[i]);
 			if(state) {
 				has_been_true[i] = true;
 			}
-			//printf(state ? "1" : "0");
 		}
 
 		for(int i = 0; i < COUNT; ++i) {
