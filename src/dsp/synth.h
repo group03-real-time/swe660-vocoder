ac8e9d1b990b8b538a34658b80e5fd6dbf21e35d
bwall4 <bwall4@gmu.edu>
2024-04-22 12:39:19 -0400

    More comments

--- src/dsp/synth.h
+++ src/dsp/synth.h
@@ -4,9 +4,18 @@
 #include "dsp.h"
 #include "audio_params.h"
 
+/** The maximum number of voices impacts the performance of the synth. */
 #define MAX_SYNTH_VOICES 4
+
+/** 
+ * The number of notes mainly impacts the size of the array of note frequency
+ * data. 
+ */
 #define NUMBER_OF_NOTES 64
 
+/**
+ * Defines the states used in the ADSR state machine for the synthesizer.
+ */
 typedef enum {
 	SYNTH_ATTACK,
 	SYNTH_DECAY,
@@ -15,32 +24,67 @@ typedef enum {
 } synth_envelope_state;
 
 typedef struct {
+	/* Which note this synth is playing. */
 	int note;
 
 	/* Used for voice-stealing */
 	uint32_t age;
 
+	/* The most recently computed sample */
 	dsp_num sample;
+
+	/* The computed ADSR envelope of the synth */
 	dsp_num envelope;
 
+	/* Internal state used for generating white noise */
 	dsp_num white_noise_generator;
 
+	/* The phase of the oscillator */
 	dsp_num phase;
+	/* How much the phase is incremented per sample. Corresponds to note frequency. */
 	dsp_num phase_step;
 
+	/* The state of the voice's ADSR. */
 	synth_envelope_state state;
 } synth_voice;
 
+/**
+ * Defines the state for a synthesizer. Similar to the vocoder, this should 
+ * generally be statically allocated somewhere for efficiency.
+ */
 typedef struct {
+	/* The next age value to assign to a voice, for voice-stealing. */
 	uint32_t next_age;
+
+	/* The array of all synth voices. */
 	synth_voice voices[MAX_SYNTH_VOICES];
 } synth;
 
+/**
+ * Initializes the synthesizer with the necessary state to start playing notes.
+ */
 void synth_init(synth *syn);
 
+/**
+ * Presses a new note on the synthesizer. Will steal a voice if necessary.
+ * 
+ * Note that, at the moment, only one copy of a given note should ever be played
+ * at a time -- that is, synth_press(..., n) should not be called twice without
+ * an intermediate synth_release(..., n). This works perfectly fine with the
+ * existing button keyboard design.
+ */
 void synth_press(synth *syn, int note);
+
+/**
+ * Releases the given note on the synthesizer. Causes the associated voice to
+ * go into SYNTH_RELEASE state.
+ */
 void synth_release(synth *syn, int note);
 
+/**
+ * Computes the next sample for the given synth, given the specified audio
+ * parameters.
+ */
 dsp_num synth_process(synth *syn, audio_params *ap);
 
 #endif
--- src/dsp/vocoder.h
+++ src/dsp/vocoder.h
@@ -4,19 +4,47 @@
 #include "dsp.h"
 #include "bpf.h"
 
+/**
+ * The vocoder struct. Contains all the state needed to perform the vocoding
+ * over time (because IIR filters are stateful).
+ * 
+ * Should generally be stack-allocated or otherwise statically allocated for
+ * efficiency.
+ */
 typedef struct {
+	/** The BPFs for the modulator signal. */
 	bpf_cascaded_biquad mod_filters[VOCODER_BANDS];
+	/** The BPFs for the carrier signal. */
 	bpf_cascaded_biquad car_filters[VOCODER_BANDS];
+	/** The envelope followers for each filtered modulator signal. */
 	dsp_num envelope_follow[VOCODER_BANDS];
 
+	/**
+	 * The input array for the filters. This is used to keep the filter chain
+	 * efficient (the input to each one is the output of the previous), but
+	 * someone needs to store the top-level input -- so it's stored here.
+	 */
 	dsp_num mod_x[3];
 	dsp_num car_x[3];
 
+	/** 
+	 * Envelope followers for the overall signal. Used to make the overall
+	 * gain of the carrier vaguely match the modulator.
+	 */
 	dsp_num mod_ef;
 	dsp_num sum_ef;
 } vocoder;
 
+/**
+ * Initializes the vocoder with all the necessary state for it to process
+ * a signal through vc_process.
+ */
 void vc_init(vocoder *v);
+
+/**
+ * Computes a single sample run through the vocoder. Requires an input for both
+ * the modulator signal and the carrier signal. Returns the vocoded signal.
+ */
 dsp_num vc_process(vocoder *v, dsp_num modulator, dsp_num carrier);
 
 #endif
