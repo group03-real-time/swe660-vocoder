065e1f97ca8d7af12e68d4e10ed87af3696465e8
bwall4 <bwall4@gmu.edu>
2024-04-22 12:21:34 -0400

    Slight improvements to main

--- src/main.c
+++ src/main.c
@@ -22,20 +22,25 @@ main_app(int argc, char **argv) {
 	/* utsname */
 	app_show_utsname();
 
+	/* Use the loop for nicer exiting */
 	app_init_loop();
 
 	vocoder voc;
-	vc_init(&voc);
+	vc_init(&voc); /* vocoder */
 
 	synth syn;
-	synth_init(&syn);
+	synth_init(&syn); /* synth */
 
 	audio_params params;
 	audio_params_default(&params);
+
+	/* We're using the multiplexer here, so start it up */
 	audio_params_init_multiplexer();
 
+	/* Also using the buttons */
 	init_button_arr();
 
+	/* These should be called as close as possible to when we start the loop */
 	pru_audio_prepare_writing();
 	pru_audio_prepare_reading();
 
@@ -43,29 +48,32 @@ main_app(int argc, char **argv) {
 	int button_tick_count = 0;
 
 	while(app_running) {
-		/* Read the modulator */
-		uint32_t modulator = pru_audio_read();
-
+		/* Update button and audio params before the main DSP code to slightly
+		 * reduce latency */
 		button_tick_count += 1;
 		if(button_tick_count >= BUTTON_READ_RATE) {
 			button_tick_count = 0;
 			button_tick(&syn, false);			
 		}
 
-		dsp_num carrier = synth_process(&syn, &params);
-
-		dsp_num out = vc_process(&voc, modulator, carrier);
-
-		/* Apply output gain */
-		out = dsp_mul(out, params.output_gain);
-
-		pru_audio_write(out);
-
 		audio_param_tick += 1;
 		if(audio_param_tick >= AUDIO_PARAM_TICK_RATE) {
 			audio_param_tick = 0;
 			audio_params_tick_multiplexer(&params, false);
 		}
+
+		/* Read the modulator signal from the microphone */
+		uint32_t modulator = pru_audio_read();
+
+		/* Compute the carrier signal from the synthesizer */
+		dsp_num carrier = synth_process(&syn, &params);
+
+		/* The output signal is vocoded */
+		dsp_num out = vc_process(&voc, modulator, carrier);
+
+		/* Apply output gain then write to the PRU */
+		out = dsp_mul(out, params.output_gain);
+		pru_audio_write(out);	
 	}
 
 	return 0;
