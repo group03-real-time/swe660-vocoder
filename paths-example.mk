94840fc28146094879ef8d946fd19ed6c852ba69
bwall4 <bwall4@gmu.edu>
2024-04-22 23:54:44 -0400

    Add paths-example.mk

---
+++ paths-example.mk
@@ -0,0 +1,15 @@
+# If you are trying to build the project on x86, you will need a copy of the
+# PRU compiler and PRU support libraries installed somewhere.
+#
+# Once you have the compiler and libraries installed, copy this file to
+# 'paths.mk' to have it automatically included in the Makefile, and edit it
+# to reflect the install path of these tools on your system.
+#
+# Note that paths.mk is in the .gitignore, so it will not be committed.
+
+
+
+# One possibility is that you installed these tools in your home directory.
+# These paths are simply examples. Definitely change them if needed.
+PRU_CGT=/home/myname/ti-cgt-pru_2.3.3
+PRU_SUPPORT=/home/myname/pru-software-support-package
